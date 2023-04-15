b38e327e576e11d3cea8ebba12e344bed2d72b83
Aleksey Koshelev <koshelev.alexej@gmail.com>
2023-04-15 19:43:39 +0700

    Uncompleted code

--- src/Main.java
+++ src/Main.java
@@ -14,5 +14,8 @@ public class Main {
             System.out.println("i = " + i);
             System.out.println();
         }
+        System.out.println();
+        int a = "";
+        System.out.println;
     }
 }
